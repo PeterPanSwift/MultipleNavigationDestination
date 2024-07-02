d11f7d68377950fdbe2d1569492b622db047cb8d
peter pan <apppeterpan@gmail.com>
2024-07-02 20:07:21 +0800

    Initial Commit

---
+++ MultipleNavigationDestination/MultipleNavigationDestinationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultipleNavigationDestinationApp.swift
+//  MultipleNavigationDestination
+//
+//  Created by SHIH-YING PAN on 2024/7/2.
+//
+
+import SwiftUI
+
+@main
+struct MultipleNavigationDestinationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
